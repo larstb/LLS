515424b84e7a0d6ecd01b572d1ac85099d85df7e
larstb <lars.bilek@gmx.at>
2023-12-20 15:02:27 +0100

    added bean validation

--- backend/src/main/java/at/ltb/apprenticedeliverysystem/core/user/dto/UpdateUserDTO.java
+++ backend/src/main/java/at/ltb/apprenticedeliverysystem/core/user/dto/UpdateUserDTO.java
@@ -1,16 +1,18 @@
 package at.ltb.apprenticedeliverysystem.core.user.dto;
 
 import at.ltb.apprenticedeliverysystem.core._common.request.BaseRequest;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 public record UpdateUserDTO(
-        String id,
-        Integer version,
-        String firstname,
-        String lastname,
+        @NotBlank String id,
+        @NotBlank @Min(0) Integer version,
+        @NotBlank String firstname,
+        @NotBlank String lastname,
         String status,
         String phoneNumber,
         String location,
         String iban,
         String paypalLink,
-        String email) implements BaseRequest {
+        @NotBlank String email) implements BaseRequest {
 }
--- backend/src/main/java/at/ltb/apprenticedeliverysystem/core/user/dto/UpdateUserPortalDTO.java
+++ backend/src/main/java/at/ltb/apprenticedeliverysystem/core/user/dto/UpdateUserPortalDTO.java
@@ -2,20 +2,24 @@ package at.ltb.apprenticedeliverysystem.core.user.dto;
 
 import at.ltb.apprenticedeliverysystem.core._common.request.BaseRequest;
 import at.ltb.apprenticedeliverysystem.core._common.role.RoleEnum;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record UpdateUserPortalDTO(
-        String id,
-        Integer version,
-        String firstname,
-        String lastname,
+        @NotBlank String id,
+        @Min(0) Integer version,
+        @NotBlank String firstname,
+        @NotBlank String lastname,
         String status,
         String phoneNumber,
         String location,
         String iban,
         String paypalLink,
-        String email,
-        Boolean enabled,
-        List<RoleEnum> roles) implements BaseRequest {
+        @NotBlank String email,
+        @NotNull Boolean enabled,
+        @Size(min = 1) List<RoleEnum> roles) implements BaseRequest {
 }
