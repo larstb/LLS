63ea147132a8cd91a9b1721a9906178654f9f7aa
larstb <lars.bilek@gmx.at>
2023-12-20 15:36:28 +0100

    fixed codacy and added frontend module config

--- backend/src/main/java/at/ltb/apprenticedeliverysystem/core/user/controller/UserController.java
+++ backend/src/main/java/at/ltb/apprenticedeliverysystem/core/user/controller/UserController.java
@@ -7,7 +7,11 @@ import at.ltb.apprenticedeliverysystem.core.user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
