721d593bfbfe09aab4155c9d28e55a1c00386dfc
larstb <lars.bilek@gmx.at>
2023-12-21 12:44:33 +0100

    codacy

--- backend/src/main/java/at/ltb/apprenticedeliverysystem/core/user/api/AdminModUserManagementResource.java
+++ backend/src/main/java/at/ltb/apprenticedeliverysystem/core/user/api/AdminModUserManagementResource.java
@@ -7,7 +7,12 @@ import at.ltb.apprenticedeliverysystem.core.user.dto.UserDetailDTO;
 import at.ltb.apprenticedeliverysystem.core.user.dto.UserOverviewDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
