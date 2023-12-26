8fdb222ae491037c5079c42dba51029f14aabb45
larstb <lars.bilek@gmx.at>
2023-12-26 18:44:00 +0100

    fixed dashboard and management-dashboard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import {authGuard} from "./shared/guard/auth.guard";
 import {portalUserResolver} from "./core/header/components/resolver/portal-user.resolver";
+import {PortalDashboardComponent} from "./portal/portal-dashboard/portal-dashboard.component";
 
 const routes: Routes = [
-  { path: '', component: AppComponent, canActivate: [authGuard], resolve: [portalUserResolver] },
-  { path: 'management', loadChildren: () => import('./management/management.module').then(m => m.ManagementModule) },
-  { path: '**', redirectTo: '' },
+  {
+    path: '',
+    component: PortalDashboardComponent,
+    canActivate: [authGuard],
+    resolve: [portalUserResolver],
+  },
+  {
+    path: 'management',
+    loadChildren: () => import('./management/management.module').then(m => m.ManagementModule),
+    canActivate: [authGuard],
+    resolve: [portalUserResolver],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
 
 @NgModule({
---
+++ frontend/src/app/portal/portal-dashboard/portal-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portal-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './portal-dashboard.component.html',
+  styleUrl: './portal-dashboard.component.css'
+})
+export class PortalDashboardComponent {
+
+}
